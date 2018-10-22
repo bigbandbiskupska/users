9efddccb4d3a61842d3df979db76b7bb5f134fe8
Krystof Tulinger <k.tulinger@seznam.cz>
2018-10-22 20:05:36 +0200

    git 1.0.1 and docker updates

--- docker/start.sh
+++ docker/start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cd /app && \
+git config --global url."https://github.com/".insteadOf "git@github.com:" && \
 php /usr/bin/composer.phar --no-interaction update && \
+git config --global url."git@github.com".insteadOf "https://github.com/" && \
 echo "Serving on 0.0.0.0:80" && \
 php -S 0.0.0.0:80 -t www
 
