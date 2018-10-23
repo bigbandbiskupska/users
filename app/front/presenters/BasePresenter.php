5f31a52156e85c5d17f271e8ff238dc9746aebe4
Krystof Tulinger <k.tulinger@seznam.cz>
2018-10-23 09:43:41 +0200

    adding update time

--- app/front/presenters/BasePresenter.php
+++ app/front/presenters/BasePresenter.php
@@ -11,13 +11,37 @@ namespace App\FrontModule\Presenters;
 
 
 use Tulinkry\Application\UI\Presenter;
+use Tulinkry\Services\ParameterService;
 
 class BasePresenter extends Presenter
 {
+
+    /**
+     * @var ParameterService
+     * @inject
+     */
+    public $parameters;
+
     public function actionOut()
     {
         $this->getUser()->logout();
         $this->flashMessage('Odhlášení bylo úspěšné.');
         $this->redirect(':User:Homepage:');
+    }
+
+    public function startup() {
+        parent::startup();
+        $timestamp = $this->parameters->params['appDir'] . DIRECTORY_SEPARATOR . 'timestamp';
+        if(file_exists($timestamp)) {
+            $this->template->lastUpdated = DateTime::from(@filemtime($timestamp));
+        }
+    }
+
+    public static function update() {
+        $timestamp = APP_DIR . DIRECTORY_SEPARATOR . 'timestamp';
+        if(file_exists($timestamp)) {
+            FileSystem::delete($timestamp);
+        }
+        FileSystem::write($timestamp, "");
     }
 }
