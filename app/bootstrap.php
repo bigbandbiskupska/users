ab77490a20d66cc6bc7d3e6e112bc047254894f2
Krystof Tulinger <k.tulinger@seznam.cz>
2018-10-23 09:49:03 +0200

    adding APP_DIR constant

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,6 +2,8 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+define("APP_DIR", __DIR__);
+
 $configurator = new Nette\Configurator;
 $configurator->setDebugMode(array('172.18.0.1', '172.20.0.1', '172.19.0.1', '127.0.0.1', '85.70.17.135', '213.220.225.67'));
 $configurator->enableDebugger(__DIR__ . '/../log');
